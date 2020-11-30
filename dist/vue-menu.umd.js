3a6e3a590b2563a5f383366b79f1e58e8c95a8bc
xubowen <bowen.xu@bangdao-tech.com>
2020-11-30 15:56:41 +0800

    fix: x

--- dist/vue-menu.umd.js
+++ dist/vue-menu.umd.js
@@ -2407,7 +2407,7 @@ var es_function_name = __webpack_require__("b0c0");
 // EXTERNAL MODULE: ./src/components/VueMenu/index.less
 var VueMenu = __webpack_require__("1372");
 
-// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"392f06d8-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/@vue/cli-plugin-babel/node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/VueMenu/index.vue?vue&type=template&id=5969d00c&
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"24661504-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/@vue/cli-plugin-babel/node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/VueMenu/index.vue?vue&type=template&id=5969d00c&
 var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"sm-menu"},[_c('sm-menu',{attrs:{"active":_vm.currentValue,"items":_vm.menu},on:{"on-click":_vm.triggerClick}})],1)}
 var staticRenderFns = []
 
@@ -2417,7 +2417,7 @@ var staticRenderFns = []
 // EXTERNAL MODULE: ./node_modules/core-js/modules/es.array.map.js
 var es_array_map = __webpack_require__("d81d");
 
-// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"392f06d8-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/@vue/cli-plugin-babel/node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/VueMenu/Menu.vue?vue&type=template&id=e9675ae6&
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"24661504-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/@vue/cli-plugin-babel/node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/VueMenu/Menu.vue?vue&type=template&id=e9675ae6&
 var Menuvue_type_template_id_e9675ae6_render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"chid-nav-wrap"},[(_vm.items)?_c('ul',{class:{ 'chid-nav': true }},_vm._l((_vm.items),function(cm){return _c('li',{key:cm.name,class:{
         active: _vm.active && _vm.active.length ? _vm.active[0] === cm.name : false
       }},[_c('div',{on:{"click":function($event){return _vm.triggerClick(cm)}}},[_c('a',[_vm._v(_vm._s(cm.title))]),(cm.items)?_c('vue-menu',{attrs:{"active":_vm.nextActive,"parent":cm,"items":cm.items},on:{"on-click":_vm.childClick}}):_vm._e()],1)])}),0):_vm._e()])}
